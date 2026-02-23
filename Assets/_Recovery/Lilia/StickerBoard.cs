4bc5f7c09ac302de9cd27556bb40f1f7ec0b9113
YazanShannek <99140793+YazanShannek@users.noreply.github.com>
2026-02-23 15:53:29 +0300

    Stickerssssss

--- Assets/_Recovery/Lilia/StickerBoard.cs
+++ Assets/_Recovery/Lilia/StickerBoard.cs
@@ -1,10 +1,46 @@
+using DG.Tweening;
+using System.Collections;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
-public class StickerBoard : MonoBehaviour
-{
-    public void OpenBoard()
-    {
-        
+public class StickerBoard : MonoBehaviour {
+  [SerializeField] GameObject board;
+  [SerializeField] StickyNoteScript [] sticky;
+  [SerializeField] Image m;
+  float e = 0;
+
+  public static StickerBoard Instance;
+
+  public StickerBoard instance;
+
+  private void Start() {
+    if (Instance == null) {
+      instance = GameObject.FindFirstObjectByType<StickerBoard>();
+      Instance = instance;
     }
+  }
+
+
+  public void OpenBoard(BoxScript.ID id, string objName, string objDescription) {
+    board.transform.DOLocalMoveY(0, 1).SetEase(Ease.InCirc).OnComplete(() => {
+      int r =Random.Range(0,sticky.Length);
+      
+      while (sticky[r].getSet()) {
+        r = Random.Range(0, sticky.Length);
+      }
+
+      string temp = objName + " " + objDescription;
+
+      sticky[r].changeText(objName);
+      Invoke(nameof(open), 0.1f);
+    });
+
+  }
+
+  void open() {
+    e+=0.05f;
+    m.material.SetFloat("_PageCurl_movement_1", e);
+    Invoke(nameof(open), 0.1f);
+  }
+
 }
--- Assets/_Recovery/Lilia/StickyNoteScript.cs
+++ Assets/_Recovery/Lilia/StickyNoteScript.cs
@@ -1,6 +1,20 @@
-using UnityEngine; 
+using UnityEngine;
+using TMPro;
+using System;
 
 public class StickyNoteScript : MonoBehaviour
 {
-     
+  public TextMeshProUGUI text;
+  private bool set;
+
+  public void changeText(string text) {
+    this.text.text = text;
+    set = true;
+  }
+
+  public bool getSet() {
+    return set;
+  }
+
+  
 }
