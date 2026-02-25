8da7e71cd6cc6fb80a10729f7753cdb3010a95d3
rama selwadi <22110167@htu.edu.jo>
2026-02-25 22:24:23 +0300

    fixed the check for the stickers once they are collected the stickers checkbox gets checked and the tasks panel appears to show th player his/her progress

--- Assets/_Recovery/Lilia/BoxScript.cs
+++ Assets/_Recovery/Lilia/BoxScript.cs
@@ -6,10 +6,24 @@ public class BoxScript : MonoBehaviour {
   [SerializeField] string objectName;
   [SerializeField] string objectDescription;
 
-  private void OnTriggerEnter(Collider other) {
-    if (other.CompareTag("Player")) {
-      StickerBoard.Instance.OpenBoard(id,objectName,objectDescription);
-      Destroy(gameObject);
+ 
+    private bool isQuitting = false;
+
+    // Unity calls this when you hit stop in the editor. 
+    // We need this so it doesn't count as "collecting" when you close the game.
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
     }
-  }
+
+    // Because BoxScript calls "Destroy(gameObject)", Unity will automatically 
+    // trigger OnDestroy() on this script right before the object vanishes.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StickerBoard.Instance.OpenBoard(id, objectName, objectDescription);
+            Destroy(gameObject);
+        }
+    }
 }
